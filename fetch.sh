dffe19feccb3e46aaea275f0f6b18d76ebfd1c62
Wenjie Guo <guowj12@gmail.com>
2018-07-05 17:03:14 +0800

    add fetch.sh

---
+++ fetch.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+APP_REPO="<>/jboss-7.1.1.Final-docker"
+
+mkdir jboss-as/maitai/etc
+wget -O jboss-as/maitai/etc/maitai.conf $APP_REPO/jboss-as/maitai.conf
+wget -O jboss-as/standalone-full.xml $APP_REPO/jboss-as/standalone-full.xml
+
+mkdir jboss-app
+wget -O jboss-app/maitai-server-ear.ear $APP_REPO/jboss-app/maitai-server-ear.ear
